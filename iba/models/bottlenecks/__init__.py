e3f3217f92ba1a36d598064eccc5c55a87e5e206
sandylaker <lfc199471@gmail.com>
2021-04-19 00:07:01 +0200

    add base classes for the bottleneck modules

--- iba/models/bottlenecks/__init__.py
+++ iba/models/bottlenecks/__init__.py
@@ -1,6 +1,7 @@
 from .base_iba import BaseIBA
 from .base_feat_iba import BaseFeatureIBA
 from .base_input_iba import BaseInputIBA
+from .vision_feat_iba import VisionWelfordEstimator
 from .vision_feat_iba import VisionFeatureIBA
 from .vision_input_iba import VisionInputIBA
 from .builder import FEATURE_IBAS, INPUT_IBAS, build_feat_iba, build_input_iba

3fdd0a0fd301be61a5e7d93f739726eabad3c23a
sandylaker <yawei.li@tum.de>
2021-04-22 16:00:11 +0200

    add base classes for GAN, Attributor

--- iba/models/__init__.py
+++ iba/models/__init__.py
@@ -1,7 +1,5 @@
-from .gan import Generator, Discriminator, WGAN_CP
-from .net import Attributor
-from .pytorch import tensor_to_np_img, _SpatialGaussianKernel, TorchWelfordEstimator, _IBAForwardHook, IBA
-from .pytorch_img_iba import ImageIBA
-from .utils import WelfordEstimator, load_monkeys, plot_saliency_map, _to_saliency_map, to_saliency_map
+from .utils import plot_saliency_map, _to_saliency_map, to_saliency_map
 from .model_zoo import build_classifiers, get_module
-from .custom_models import *
+from .bottlenecks import *
+from .gans import *
+from .estimators import *

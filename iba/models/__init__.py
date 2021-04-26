b9b2b46815338e5fdabc090b0f2b68cac0c66898
sandylaker <lfc199471@gmail.com>
2021-04-26 22:43:46 +0200

    update train script and configs
    - update train script
    - adapt the configs to be compatible with the new model structure

--- iba/models/__init__.py
+++ iba/models/__init__.py
@@ -1,5 +1,5 @@
-from .utils import plot_saliency_map, _to_saliency_map, to_saliency_map
-from .model_zoo import build_classifiers, get_module
+from .attributors import *
 from .bottlenecks import *
-from .gans import *
 from .estimators import *
+from .gans import *
+from model_zoo import get_module, build_classifiers

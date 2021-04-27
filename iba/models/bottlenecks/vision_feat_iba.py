a011e162ddad0befa7cc20f8825a97e820b92eff
sandylaker <yawei.li@tum.de>
2021-04-27 13:53:34 +0200

    fix bugs and add more configs

--- iba/models/bottlenecks/vision_feat_iba.py
+++ iba/models/bottlenecks/vision_feat_iba.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import warnings
 from ..utils import get_tqdm, _SpatialGaussianKernel
 from .base_feat_iba import BaseFeatureIBA

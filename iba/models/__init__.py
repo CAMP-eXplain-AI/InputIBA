e82d1f8414a98088dbf676f286ff5612efc6038d
sandylaker <lfc199471@gmail.com>
2021-04-30 23:24:03 +0200

    fix bug 'ResNet is not in the registry'

--- iba/models/__init__.py
+++ iba/models/__init__.py
@@ -2,4 +2,5 @@ from .attributors import *
 from .bottlenecks import *
 from .estimators import *
 from .gans import *
+from custom_models import *
 from .model_zoo import MODELS, get_module, build_classifiers
